e8d9a38a1e03b85a37988347b93a6ca4b7fad86a
Dhinesh <dhinesh.rbabu@gmail.com>
2019-03-15 09:45:14 +0530

    Fixed Case issue

--- RSA-encryption.py
+++ RSA-encryption.py
@@ -2,23 +2,32 @@ def encrypt(pk, plaintext):
     # Unpack the key into it's components
     key, n = pk
     # Generate the ciphertext based on the plaintext and key using a^b mod m
-    cipher = [pow(plaintext,key,n)]
+    cipher = pow(plaintext,key,n)
     return cipher
 
 if __name__ == '__main__':
     print "\nEnter the public key {e,n} for encryption:\n"
     e = int(input("Enter e: "))
     n = int(input("Enter n: "))
+    #e=58240451197
+    #n=819708416353
     nlength = len(str(n))
     public = (e, n)
     while(1):
-        message = raw_input("\nEnter the message (m): ").upper()
-        mlength = len(message)
-        if(nlength <= 2*mlength):
-            print "Size of m should be less than the size of n"
-            continue
+        message = raw_input("\nEnter the message (m): ")
+        
         mess = ''
         for i in message:
-            mess=mess+str(ord(i))
+            if( ord(i) < 100 ):
+                mess = mess + '0'+str(ord(i))
+            else:
+                mess=mess+str(ord(i))
+        print "ASCII value for encryption: "+mess
+        
+        if( n <= int(mess)):
+            print "Size of m should be less than the size of n"
+            continue
+        
         encrypted_msg = encrypt(public, int(mess))
-        print "Encrypted message: ", ''.join(map(lambda x: str(x), encrypted_msg))
+        print "Encrypted Message: "+str(encrypted_msg)
+        
